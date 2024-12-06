545dd8ca968eeb04e8e4c56705dc0e7b4bad0a66
unknown <wafiyatin@gmail.com>
2024-12-06 13:55:07 +0700

    Normalize line endings to LF

---
+++ resources/views/auth/template/footer.blade.php
@@ -0,0 +1,8 @@
+<!-- jQuery -->
+<script src="../../plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="{{asset('/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<!-- AdminLTE App -->
+<script src="{{asset('dist/js/adminlte.min.js')}}"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[UserController::class,'login'])->name('login');
+Route::get('/register',[UserController::class,'register'])->name('register');
+Route::post('/register',[UserController::class,'registerStore'])->name('register.store');
+Route::resource('users',UserController::class);
